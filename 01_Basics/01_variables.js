fe4ac055fe8a2239c439a9b658b6b6b39a784f9a
fizza tariq <84499967+fizza603@users.noreply.github.com>
2025-08-05 18:45:18 +0000

    initial start

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 14437
+let accountEmail = "tariqfizza70@gmail.com"
+var accountPassword = "12345"
+accountCity = "Karachi"
+
+// accountId = 25
+accountEmail = "tqfz@gmail.com"
+accountPassword = "231442"
+accountCity = "Lahore"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+
+
+
