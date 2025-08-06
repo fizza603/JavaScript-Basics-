47e85347222b62b781ef8622963f8142c41c8930
fizza tariq <84499967+fizza603@users.noreply.github.com>
2025-08-06 06:56:01 +0000

    DataTypes

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -2,14 +2,18 @@ const accountId = 14437
 let accountEmail = "tariqfizza70@gmail.com"
 var accountPassword = "12345"
 accountCity = "Karachi"
+let accountState;
 
 // accountId = 25
 accountEmail = "tqfz@gmail.com"
 accountPassword = "231442"
 accountCity = "Lahore"
-console.log(accountId);
-console.table([accountId,accountEmail,accountPassword,accountCity]);
 
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
 
+/*
+Prefer not to use var
+Because of issue in block scope and functional scope
+*/
 
 
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,50 @@
+"use strict"; //treat all JS Code as newer version
+
+
+
+let Name = "Fizza Tariq"
+let age = 22
+let isLogin = false
+let State;
+
+
+// number = 2 to power of 53
+// bigint
+//string => ""
+// Boolean => true/false
+//null => stand alone value(representation of empty value)
+//undefined => variable declare but no value is assigned
+//symbol => unique
+
+//object
+// console.table([Name,age,isLogin,State]);
+
+// console.log(typeof null);//output:object
+// console.log(typeof undefined);  //output:undefined
+ 
+let score = "33qdas"
+
+// console.log(typeof score);
+
+let ValueinNumber = Number(score)
+// console.log(typeof ValueinNumber);
+// console.log(ValueinNumber);
+
+
+// "33" => 33
+// "33qsd" => NaN
+// true=> 1 , false => 0
+let isLoggedIn  = "fizza"
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanisLoggedIn)
+
+
+// 1 => true ; 0 => false
+// "" => false ; "fizza" => true
+
+
+
+
+
+
+
